5e37cb78101647cda92dcde702a2501f3069bc41
Luis Carlos Pando <hey@luiscarlospando.com>
2023-10-26 17:08:27 -0600

    Armar vista de categorías

--- category.php
+++ category.php
@@ -1,19 +1,115 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<h1><?php esc_html_e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
-
-			<?php get_template_part( 'loop' ); ?>
-
-			<?php get_template_part( 'pagination' ); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<section id="productos" class="category py-60">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col text-center">
+                    <h1
+                        data-aos="fade-up"
+                    >
+                        Nuestros <span class="texto-rojo">productos.</span>
+                    </h1>
+                    <p
+                        class="fondo-azul my-4"
+                        data-aos="fade-up"
+                        data-aos-delay="100"
+                    >
+                        La razón de nuestra alta calidad
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <nav>
+                        <div class="nav nav-pills" id="nav-tab" role="tablist">
+                            <button
+                                class="nav-link active"
+                                id="nav-todos-tab" data-bs-toggle="tab"
+                                data-bs-target="#nav-todos"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-todos"
+                                aria-selected="true"
+                                data-aos="fade-up"
+                                data-aos-delay="200"
+                            >
+                                Todos los productos
+                            </button>
+                            <button
+                                class="nav-link"
+                                id="nav-chihuahua-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-chihuahua"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-chihuahua"
+                                aria-selected="false"
+                                data-aos="fade-up"
+                                data-aos-delay="300"
+                            >
+                                Chihuahua
+                            </button>
+                            <button
+                                class="nav-link"
+                                id="nav-gouda-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-gouda"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-gouda"
+                                aria-selected="false"
+                                data-aos="fade-up"
+                                data-aos-delay="400"
+                            >
+                                Goudá
+                            </button>
+                            <button
+                                class="nav-link"
+                                id="nav-oaxaca-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-oaxaca"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-oaxaca"
+                                aria-selected="false"
+                                data-aos="fade-up"
+                                data-aos-delay="500"
+                            >
+                                Oaxaca
+                            </button>
+                            <button
+                                class="nav-link"
+                                id="nav-manchego-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-manchego"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-manchego"
+                                aria-selected="false"
+                                data-aos="fade-up"
+                                data-aos-delay="600"
+                            >
+                                Manchego
+                            </button>
+                            <button
+                                class="nav-link"
+                                id="nav-panela-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-panela"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-panela"
+                                aria-selected="false"
+                                data-aos="fade-up"
+                                data-aos-delay="700"
+                            >
+                                Panela
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
