35ddbdc59b955f99c858c2c56ef25025419c62ba
Luis Carlos Pando <hey@luiscarlospando.com>
2023-10-26 11:57:05 -0600

    Estructura tentativa

--- single.php
+++ single.php
@@ -16,18 +16,11 @@
 			</div>
 			<div class="row">
 				<div class="col">
-					<?php esc_html_e( 'Por', 'html5blank' ); ?> <?php the_author_posts_link(); ?> <?php esc_html_e( 'a la(s)', 'html5blank' ); ?> <time datetime="<?php the_time( 'Y-m-d' ); ?> <?php the_time( 'H:i' ); ?>"><?php the_date(); ?> <?php the_time(); ?></time>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
 					<?php the_content(); // Dynamic Content. ?>
 
 					<?php the_category( ', ' ); // Separated by commas. ?> <?php the_tags( __( 'Etiquetas: ', 'html5blank' ), ', ', '<br>' ); // Separated by commas with a line break at the end. ?>
 
 					<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
-
-					<?php comments_template(); ?>
 				</div>
 			</div>
 		</div>
