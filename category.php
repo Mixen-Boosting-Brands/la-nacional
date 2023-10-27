174abad6172aae661c31dd3743a671b2c22b3db7
Luis Carlos Pando <hey@luiscarlospando.com>
2023-10-27 10:15:46 -0600

    Desplegar un listados con todas las tags existentes en el sitio

--- category.php
+++ category.php
@@ -18,6 +18,17 @@
                     </p>
                 </div>
             </div>
+			<div class="row">
+				<ul>
+					<?php
+					$tags = get_tags();
+					if ( $tags ) :
+						foreach ( $tags as $tag ) : ?>
+							<li><a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" title="<?php echo esc_attr( $tag->name ); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</ul>
+			</div>
             <div class="row">
                 <div class="col">
                     <nav>
