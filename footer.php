3da9a61e40918aa754f17bef801fe769008fae75
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-02 13:54:39 -0600

    Implementar funcionalidad básica en modales

--- footer.php
+++ footer.php
@@ -103,7 +103,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <img src="" alt="" class="thumbnail img-fluid">
+                    <img src="" alt="" id="thumbnail" class="img-fluid">
                     <p class="gramaje"></p>
                     <p class="ingredientes"></p>
                 </div>
