c15a3f21d4043ef3995c3a0085b40d065f006ab8
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-03 12:46:40 -0600

    Implementar funcionalidad básica en modales

--- category.php
+++ category.php
@@ -95,8 +95,9 @@
                                         </span>
                                         <?php endif; ?>
                                         <div class="card-img-top">
-                                            <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php echo get_sub_field('gramaje'); ?>" data-bs-ingredientes="<?php echo get_sub_field('ingredientes'); ?>">
+                                            <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php if( get_sub_field('peso_del_producto') ): ?><?php echo get_sub_field('gramaje'); ?><?php endif; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes'); ?><?php endif; ?>">
                                                 <?php the_post_thumbnail('thumb-producto', array('class' => 'img-fluid')); ?>
+                                                <?php if( get_sub_field('peso_del_producto') ): ?><?php echo get_sub_field('gramaje'); ?><?php endif; ?>
                                             </a>
                                         </div>
                                         <div class="card-body">
