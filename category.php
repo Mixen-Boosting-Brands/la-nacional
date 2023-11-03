324d788ee972d75ce859da3d432c143ce4849664
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-03 13:14:11 -0600

    Implementar funcionalidad básica en modales

--- category.php
+++ category.php
@@ -98,18 +98,19 @@
                                             <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php if( get_sub_field('peso_del_producto') ): ?><?php echo get_sub_field('gramaje'); ?><?php endif; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes'); ?><?php endif; ?>">
                                                 <?php the_post_thumbnail('thumb-producto', array('class' => 'img-fluid')); ?>
 
-                                                <?php
-                                                    $gramaje = get_sub_field('gramaje');
-                                                    echo $gramaje;
-                                                    if( $gramaje ):
-                                                ?>
-                                                    <ul>
-                                                        <?php foreach( $gramajes as $gramaje ): ?>
-                                                            <li><?php echo $gramaje; ?></li>
-                                                        <?php endforeach; ?>
-                                                    </ul>
+                                                <?php if( get_sub_field('peso_del_producto') ): ?>
+                                                    <?php
+                                                        $gramaje = get_sub_field('gramaje');
+                                                        echo $gramaje;
+                                                        if( $gramaje ):
+                                                    ?>
+                                                        <ul>
+                                                            <?php foreach( $gramajes as $gramaje ): ?>
+                                                                <li><?php echo $gramaje; ?></li>
+                                                            <?php endforeach; ?>
+                                                        </ul>
+                                                    <?php endif; ?>
                                                 <?php endif; ?>
-
 
                                             </a>
                                         </div>
