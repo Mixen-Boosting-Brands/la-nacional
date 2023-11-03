132d45a78cb801d9779d9229cc2595a56f3c87bc
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-03 14:14:12 -0600

    Implementar funcionalidad básica en modales

--- category.php
+++ category.php
@@ -98,7 +98,7 @@
                                             <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php if( get_sub_field('peso_del_producto') ): ?><?php echo get_sub_field('gramaje'); ?><?php endif; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes'); ?><?php endif; ?>">
                                                 <?php the_post_thumbnail('thumb-producto', array('class' => 'img-fluid')); ?>
 
-                                                <?php if( get_sub_field('peso_del_producto') ): ?>
+                                                <?php if( get_sub_field('peso_del_producto') ): echo get_sub_field('peso_del_producto'); ?>
                                                     <?php
                                                         $gramaje = get_sub_field('gramaje');
                                                         echo $gramaje;
