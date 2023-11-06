a14d62a304fc8e25a705d72ee7301d9f6a53e99e
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-06 14:33:25 -0600

    Ajustes modales

--- footer.php
+++ footer.php
@@ -101,7 +101,7 @@
                 <div class="modal-header">
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body text-center">
                     <div class="container">
                         <div class="row">
                             <div class="col">
