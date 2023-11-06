65b18848fe9efcc0c932a5da471a9b2df84ba020
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-06 14:27:31 -0600

    Ajustes modales

--- footer.php
+++ footer.php
@@ -99,10 +99,10 @@
         <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h3 class="modal-title text-uppercase"></h3>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
+                    <h3 class="modal-title text-uppercase"></h3>
                     <img alt="" class="thumbnail img-fluid">
                     <p class="gramaje"></p>
                     <p class="ingredientes"></p>
