eafc01122f2d18f8b716df953419dd8efc574585
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-06 15:22:32 -0600

    Ajustes modales

--- category.php
+++ category.php
@@ -95,7 +95,7 @@
                                         </span>
                                         <?php endif; ?>
                                         <div class="card-img-top">
-                                            <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php $gramajes = get_sub_field('peso_del_producto')['gramaje']; if( $gramajes ): ?><?php foreach( $gramajes as $gramaje ): ?><?php $htmlContent="<span>$gramaje</span>"; echo htmlspecialchars($htmlContent, ENT_QUOTES, 'UTF-8'); ?><?php endforeach; ?><?php endif; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes_del_producto')['ingredientes']; ?><?php endif; ?>">
+                                            <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php $gramajes = get_sub_field('peso_del_producto')['gramaje']; if( $gramajes ): ?><?php foreach( $gramajes as $gramaje ): ?><?php echo '<span>' . htmlspecialchars($gramaje, ENT_QUOTES, 'UTF-8') . '</span>'; ?><?php endforeach; ?><?php endif; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes_del_producto')['ingredientes']; ?><?php endif; ?>">
                                                 <?php the_post_thumbnail('thumb-producto', array('class' => 'img-fluid')); ?>
 
                                                 
