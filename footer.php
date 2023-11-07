059e2f688c81fe7c64156f8dfe6a3577363bea52
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-07 13:30:10 -0600

    Avance hasta el medio día del 7/11/2023

--- footer.php
+++ footer.php
@@ -104,7 +104,7 @@
     </a -->
 
     <!-- Modales -->
-    <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modal" aria-hidden="true">
+    <div class="modal modal-producto fade" id="modal" tabindex="-1" aria-labelledby="modal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
