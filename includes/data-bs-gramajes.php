1b0a907f954844309dc799d0d3c488c903e8c9d6
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-06 18:31:20 -0600

    Avance hasta el 6/11/2023

---
+++ includes/data-bs-gramajes.php
@@ -0,0 +1,13 @@
+<?php
+    $gramajes = get_sub_field('peso_del_producto')['gramaje'];
+    if ($gramajes):
+        $jsonEncodedGramajes = array_map(
+            function($gramaje) {
+                return htmlspecialchars(json_encode($gramaje), ENT_QUOTES, 'UTF-8');
+            },
+            $gramajes
+        );
+
+        echo "data-gramajes='<span>" . implode('</span><span>', $jsonEncodedGramajes) . "</span>'";
+    endif;
+?>
