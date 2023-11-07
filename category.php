edd5a70d599989a9d8c4ed8e022396a9a071a88a
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-07 12:30:26 -0600

    Hacer cliqueable el nombre del producto

--- category.php
+++ category.php
@@ -100,9 +100,11 @@
                                             </a>
                                         </div>
                                         <div class="card-body">
-                                            <p class="card-text">
-                                                <?php the_title(); ?>
-                                            </p>
+                                            <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php include get_template_directory() . '/includes/data-bs-gramajes.php'; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes_del_producto')['ingredientes']; ?><?php endif; ?>">
+                                                <p class="card-text">
+                                                    <?php the_title(); ?>
+                                                </p>
+                                            </a>
                                         </div>
                                     <?php endwhile; endif; ?>
                                     </div>
@@ -157,9 +159,11 @@
                                             </a>
                                         </div>
                                         <div class="card-body">
-                                            <p class="card-text">
-                                                <?php the_title(); ?>
-                                            </p>
+                                            <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#modal" data-bs-titulo="<?php the_title(); ?>" data-bs-thumbnail-url="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" data-bs-gramaje="<?php include get_template_directory() . '/includes/data-bs-gramajes.php'; ?>" data-bs-ingredientes="<?php if( get_sub_field('ingredientes_del_producto') ): ?><?php echo get_sub_field('ingredientes_del_producto')['ingredientes']; ?><?php endif; ?>">
+                                                <p class="card-text">
+                                                    <?php the_title(); ?>
+                                                </p>
+                                            </a>
                                         </div>
                                     <?php endwhile; endif; ?>
                                     </div>
