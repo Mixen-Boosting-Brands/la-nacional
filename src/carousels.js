afeac7d9a937182c16ef6a9b1075eb6aa65c8d28
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-15 13:29:26 -0600

    Ajustes jumbotron

--- src/carousels.js
+++ src/carousels.js
@@ -30,39 +30,23 @@ const swiperJumbotron = new Swiper('.swiper-jumbotron', {
 
     // Events
     on: {
-      init: function () {
-        // Refresh AOS animations on all slides when Swiper initializes
-        this.slides.forEach(function (slide) {
-            var slideElements = slide.querySelectorAll('[data-aos]');
-            slideElements.forEach(function (element) {
-                AOS.init({
-                    offset: 50, // set the offset as desired
-                    duration: 1000, // set the duration as desired
-                });
-                AOS.refresh(element);
-            });
-        });
-    },
-    slideChange: function () {
-        console.log('slideChange event triggered');
-        console.log('Active slide index:', this.realIndex);
-        // Check if the current active slide is the second slide
-        if (this.realIndex === 1) {
-            console.log('Refreshing AOS animations on active slide');
-            // Target child elements of the active slide with the data-aos attribute
-            var activeSlideElements = this.slides[this.realIndex].querySelectorAll('[data-aos]');
-            // Loop through each child element and refresh animations
-            console.log('Active slide elements:', activeSlideElements);
-            activeSlideElements.forEach(function (element) {
-                // console.log(element)
-                AOS.init({
-                    offset: 50, // set the offset as desired
-                    duration: 1000, // set the duration as desired
-                });
-                AOS.refresh(element);
+      reachEnd: function () {
+        console.log('swiper initialized');
 
-            });
-        }
-    }
+        const circle1 = document.getElementById('circle-1');
+        circle1.classList.add("aos-animate");
+        circle1.setAttribute('data-aos','fade-left');
+        circle1.setAttribute('data-aos-delay','250');
+
+        const circle2 = document.getElementById('circle-2');
+        circle2.classList.add("aos-animate");
+        circle2.setAttribute('data-aos','fade-left');
+        circle2.setAttribute('data-aos-delay','500');
+
+        const circle3 = document.getElementById('circle-3');
+        circle3.classList.add("aos-animate");
+        circle3.setAttribute('data-aos','fade-left');
+        circle3.setAttribute('data-aos-delay','750');
+      },
     },
 });
