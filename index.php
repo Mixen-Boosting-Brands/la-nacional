80a73c616f49d29487f752aa32bd303698eaa230
Luis Carlos Pando <hey@luiscarlospando.com>
2023-11-16 16:13:08 -0600

    Crear sección 'Encuéntranos'

--- index.php
+++ index.php
@@ -151,6 +151,11 @@
                                     <div class="swiper-slide">Slide 1</div>
                                     <div class="swiper-slide">Slide 2</div>
                                     <div class="swiper-slide">Slide 3</div>
+                                    <div class="swiper-slide">Slide 4</div>
+                                    <div class="swiper-slide">Slide 5</div>
+                                    <div class="swiper-slide">Slide 6</div>
+                                    <div class="swiper-slide">Slide 7</div>
+                                    <div class="swiper-slide">Slide 8</div>
                                 </div>
 
                                 <!-- If we need scrollbar -->
